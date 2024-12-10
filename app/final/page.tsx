5744d507527d431d53c515037abbabd2cc26ad36
Himelmiah13 <Himelmiah58@gmail.com>
2024-12-10 00:25:40 +0000

    page.tsx

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -2,6 +2,10 @@ export default function Page (){
     return (
         <>
         <h1 className= "text-3xl"> Final Exam</h1>
+        <form>
+            <label htmlFor="product">Product Description </label>
+            <input type="text" name="product" id="product" />
+        </form>
         </>
     );
 }
