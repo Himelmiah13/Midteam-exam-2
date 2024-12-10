b6f9496cfc72535bb646c21217e49ce6ece73047
Himelmiah13 <Himelmiah58@gmail.com>
2024-12-10 00:31:35 +0000

    page.tsx

--- app/final/page.tsx
+++ app/final/page.tsx
@@ -4,7 +4,8 @@ export default function Page (){
         <h1 className= "text-3xl"> Final Exam</h1>
         <form>
             <label htmlFor="product">Product Description </label>
-            <input type="text" name="product" id="product" className="text-black bg-red-400"/>
+            <input type="text" name="product" id="product" className="text-yellow bg-red-700"/>
+            <button type="submit">Save</button>
         </form>
         </>
     );
